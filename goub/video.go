35dc8724e846d6a5f49da3138637bbeab5738856
PrimeApp AppleID <admin@primeapp.ru>
2015-03-16 16:54:43 +0300

    updated coub model

--- goub/video.go
+++ goub/video.go
@@ -6,19 +6,20 @@ type CoubService struct {
 
 type Web struct {
 	Template *string
-	// Types
-	// Version
+	Types    []string
+	Versions []string
 }
 
 type WebChunks struct {
 	Template *string
-	// Types
-	// Version
+	Types    []string
+	Versions []string
+	Chuncks  []int
 }
 
 type HTML5 struct {
 	Template *string
-	Chunks   *WebChunks
+	Chunks   []int
 }
 
 type IPhone struct {
@@ -32,29 +33,164 @@ type Mobile struct {
 	FramesCount *int
 }
 
-type Coub struct {
+type AudioVersionsChuncks struct {
+	Template *string
+	Versions []string
+	Chuncks  []int
+}
+
+type AudioVersions struct {
+	Template *string
+	Version  []string
+	Chunks   *AudioVersionsChuncks
+}
+
+type FLVAudioVersionsFile struct {
+	Template *string
+	Version  []string
+}
+
+type FLVAudioVersionsChuncks struct {
+	Template *string
+	Version  []string
+	Chuncks  []int
+}
+
+type FLVAudioVersions struct {
+	File    *FLVAudioVersionsFile
+	Chuncks *FLVAudioVersionsChuncks
+}
+
+type ImageVersions struct {
+	Template *string
+	Version  []string
+}
+
+type FirstFrameVersions struct {
+	Template *string
+	Version  []string
+}
+
+type Dimenstions struct {
+	Big   [][2]int
+	Med   [][2]int
+	Small [][2]int
+}
+
+type ExternalDownload struct {
+	Type *string
+	URL  *string
+}
+
+type AvatarVersions struct {
+	Type     *string
+	Versions []string
+}
+
+type Channel struct {
 	ID             *int
-	TYPE           *string
 	Permalink      *string
+	Description    *string
 	Title          *string
-	VisabilityType *string
-	ChannelID      *int
-	CreatedAt      *Timestamp
-	UpdatedAt      *Timestamp
-	IsDone         *bool
-	Duration       *float64
-	ViewsCount     *int
-	COTD           *bool
-	COTDAt         *Timestamp
-	Recoub         *bool
-	Like           *bool
-	RecoubsCount   *int
-	LikesCount     *int
-	RecoubTo       *int
-	Flag           *bool
-	OriginalSound  *bool
-	HasSound       *bool
-	// FileVersions
+	IFollowHim     *bool
+	FollowersCount *int
+	FollowingCount *int
+}
+
+type TrackMeta struct {
+	Year   *string
+	Album  *string
+	Title  *string
+	Artist *string
+}
+
+type AudioTrack struct {
+	ID            *int
+	Title         *string
+	URL           *string
+	Image         *string
+	ImageRetina   *string
+	ItunesURL     *string
+	AmazonURL     *string
+	BadcampURL    *string
+	SoundCloudURL *string
+	TrackName     *string
+	TrackArtist   *string
+	TrackAlbum    *string
+	Meta          *TrackMeta
+}
+
+type ExternalVideo struct {
+	ID            *int
+	Title         *string
+	URL           *string
+	Image         *string
+	ImageRetina   *string
+	ItunesURL     *string
+	AmazonURL     *string
+	BadcampURL    *string
+	SoundCloudURL *string
+}
+
+type MediaBlocks struct {
+	CoubsAudioTrack    *AudioTrack
+	CoubsExternalVideo *ExternalVideo
+}
+
+type FileVersions struct {
+	Web        *Web
+	WebChuncks *WebChunks
+	HTML5      *HTML5
+	IPhone     *IPhone
+	Mobile     *Mobile
+}
+
+type Tag struct {
+	ID    *int
+	Type  *string
+	Value *string
+}
+
+type Coub struct {
+	ID                  *int
+	TYPE                *string
+	Permalink           *string
+	Title               *string
+	VisabilityType      *string
+	ChannelID           *int
+	CreatedAt           *Timestamp
+	UpdatedAt           *Timestamp
+	IsDone              *bool
+	Duration            *float64
+	ViewsCount          *int
+	COTD                *bool
+	COTDAt              *Timestamp
+	Recoub              *bool
+	Like                *bool
+	RecoubsCount        *int
+	LikesCount          *int
+	RecoubTo            *int
+	Flag                *bool
+	OriginalSound       *bool
+	HasSound            *bool
+	FileVersions        *FileVersions
+	AudioVersions       *AudioVersions
+	FLVAudioVersions    *FLVAudioVersions
+	ImageVersions       *ImageVersions
+	FirstFrameVersions  *FirstFrameVersions
+	Dimenstions         *Dimenstions
+	AgeResticted        *bool
+	AgeRestictedByAdmin *bool
+	AllowReuse          *bool
+	Banned              *bool
+	ExternalDownload    *ExternalDownload
+	Channel             *Channel
+	PersentDone         *int
+	Tags                []Tag
+	RawVideoID          *int
+	MediaBlocks         *MediaBlocks
+	RawVideoThumbNail   *string
+	RawVideoTitle       *string
 }
 
 func (c *CoubService) Get() {}
